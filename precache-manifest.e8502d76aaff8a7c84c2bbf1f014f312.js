a518df4b23f904dca3e91025ecab71993f915ef2
Alla Sariieva <alla.cleo@gmail.com>
2020-01-03 14:43:23 +0200

    Updates

---
+++ precache-manifest.e8502d76aaff8a7c84c2bbf1f014f312.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c0a0c25ecbb715cce803126c918a0e7",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "8230f6201f990e3fbce7",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.eeeca242.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.eeeca242.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc37cefc5c6438e87bea",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.872dcad8.chunk.js"
+  },
+  {
+    "revision": "0cb2e5037341a647d556",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.791807b6.js"
+  }
+]);
